f69969fafc88a37a2f14253ae4f7964584835ae8
Zas <thezasm03@gmail.com>
2023-12-05 10:56:28 +0100

    Empiezo notificaciones

---
+++ src/pages/Notificaciones.jsx
@@ -0,0 +1,41 @@
+import { useState, useEffect } from "react";
+
+import { conseguirUsuarios } from "../services/usuarios.js";
+import { returnSessionAdmin } from "../utils/sessions.js";
+
+import Layout from "../components/Layout/Layout.jsx";
+import TablaUsuarios from "../components/Jugadores/Tabla/Tabla.jsx";
+
+import { CircularProgress } from "@nextui-org/react";
+
+function Notificaciones() {
+  const [usuarios, setUsuarios] = useState();
+  const [cargando, setCargando] = useState(true);
+  const [cambioDatos, setCambioDatos] = useState(false);
+
+  useEffect(() => {
+    returnSessionAdmin(window.localStorage.getItem("token"));
+    conseguirUsuarios(setCambioDatos).then((listaUsuarios) => {
+      setUsuarios(listaUsuarios.result);
+      setCargando(false);
+    });
+  }, [cambioDatos]);
+
+  if (cargando || localStorage.getItem("usuario") == null) {
+    return (
+      <Layout>
+        <div className="w-full h-full flex justify-center items-center mt-16">
+          <CircularProgress aria-label="Cargando..." />
+        </div>
+      </Layout>
+    );
+  }
+
+  return (
+    <Layout>
+      <TablaUsuarios listaUsuarios={usuarios} setCambioDatos={setCambioDatos} cambioDatos={cambioDatos}></TablaUsuarios>
+    </Layout>
+  );
+}
+
+export default Notificaciones;
